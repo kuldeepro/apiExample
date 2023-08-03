f63b14ad1878e0dbacbc77b2137cfb474635aab6
Kuldeep Dangi <kuldeepdangi@Kuldeeps-MacBook-Pro.local>
2023-08-03 22:00:47 +0530

    new name added

--- src/main/java/com/mad/apiExample/controllers/MainController.java
+++ src/main/java/com/mad/apiExample/controllers/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
 
-    String[] students = new String[]{"Kuldeep", "Sam", "Rohan", "Vishrut"};
+    String[] students = new String[]{"Kuldeep", "Sam", "Rohan", "Vishrut", "Mrinalini"};
 
 
     @GetMapping("/ping")
